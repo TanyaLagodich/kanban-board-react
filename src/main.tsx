aeed8cd55099069647489dbf8d38398bd0088590
Tatiana Lagodich <tanyalagodichka@gmail.com>
2026-05-04 10:47:26 +0200

    feat: supabase setup

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// remove after testing
+import { supabase } from './lib/supabase'
+const { data, error } = await supabase.from('columns').select('*')
+console.log('columns:', data)
+console.log('error:', error)
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
